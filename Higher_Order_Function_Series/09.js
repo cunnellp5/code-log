77019ab6e2a84b5ddbbbb077a0e1966762bfc025
cunnellp5 <philip.cunnell@colorado.edu>
2017-02-11 22:55:43 -0700

    warm up

--- Higher_Order_Function_Series/09.js
+++ Higher_Order_Function_Series/09.js
@@ -25,7 +25,7 @@ var list1 = [
   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
   { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
   { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
-  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
   { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
   { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
