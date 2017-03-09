59b7ae0e1b20749e059d7c5dcf43033df23e93e3
cunnellp5 <philip.cunnell@colorado.edu>
2017-03-09 09:07:15 -0700

    warmup

---
+++ AllStarCodeChallenge/18.js
@@ -0,0 +1,16 @@
+// This Kata is intended as a small challenge for my students
+//
+// All Star Code Challenge #18
+//
+// Create a function called strCount() that accepts 2 string arguments
+//  and returns an integer of the count of occurrences the 2nd argument
+//   are found in the first.
+//
+// If no occurrences can be found, a count of 0 should be returned.
+//
+// strCount('Hello', 'o') // => 1
+// strCount('Hello', 'l') // => 2
+// strCount('', 'z') // => 0
+// Note:
+// The first argument can be an empty string
+// The second string argument will always be of length 1
