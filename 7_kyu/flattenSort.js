3769a36f8359f4ee9306a1d0aba5c47a743831af
cunnellp5 <philip.cunnell@colorado.edu>
2017-04-27 21:39:01 -0400

    flatten nested arrays

---
+++ 7_kyu/flattenSort.js
@@ -0,0 +1,23 @@
+// Given a two-dimensional array of integers, return the flattened version of
+// the array with all the integers in the sorted (ascending) order.
+//
+// Example:
+//
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return
+// [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+
+function flattenAndSort(array) {
+  var flat = [].concat.apply([], array)
+  return flat.sort((a, b) => a - b)
+}
+
+
+  console.log((flattenAndSort([]), []));
+  console.log((flattenAndSort([[], [1]]), [1]));
+  console.log((flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]])));
+  console.log((flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])));
+
+
+  // var arr = [1, [2, 3], [4, 5, 6]];
+  // [].concat.apply([], arr) // -> [1, 2, 3, 4, 5, 6]
