06707ce66217db8b02d6db60b4762685a7798d63
cunnellp5 <philip.cunnell@colorado.edu>
2017-04-30 18:35:35 -0400

    code fight was tough today

--- 7_kyu/sumarray.js
+++ 7_kyu/sumarray.js
@@ -1,8 +1,30 @@
 // Your job is to add a method 'sum' to all array objects, which sums all the
 // numbers in it. You should return 0 if the array is empty.
 
-function sum (arr) {
+var arr = [1,2,3,4];
+arr.sum = function() {
   return arr.reduce((a,b) => a+b)
 }
+arr.sum();  //returns 10
 
-console.log(sum([1,2,3,4]));
+var sum = {
+  arr: [],
+  sum: function() {
+    return arr.reduce((a,b) => a+b)
+  }
+}
+
+
+
+var circle = {
+    radius: 2,
+    circumference: function() {
+        return Math.PI * 2 * this.radius;
+    },
+    area: function() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+circle.radius; // 2
+circle.area;   // function () { ...
+circle.area(); // 12.566370614359172
--- ~codefights/arcade/almostincreasingsequence.js
+++ ~codefights/arcade/almostincreasingsequence.js
@@ -10,28 +10,45 @@
 // You can remove 3 from the array to get the strictly increasing sequence [1, 2].
 //  Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 
+// function almostIncreasingSequence(sequence) {
+//   var check = sequence[0];
+//   var answer = [];
+//   var garbage = [];
+//   // pluck one of the larger numbers out
+//   for (var i = 0; i < sequence.length; i++) {
+//     check < sequence[i] ? check = sequence[i] / garbage.push(check) : answer.push(sequence[i]);
+//   }
+//   // now look for increasing sequence
+//   // var checker = 0;
+//   // var result = '';
+//   // for (var i = 0; i < answer.length; i++) {
+//   //   if (answer[i] > checker) {
+//   //     checker = answer[i]
+//   //     result = true
+//   //   } else if (answer[i] = checker) {
+//   //     result = false
+//   //     return result
+//   //   } else {
+//   //     result = false
+//   //     return result
+//   //   }
+//   // }
+//   return answer
+// }
+
 function almostIncreasingSequence(sequence) {
-  var check = sequence[0];
-  var answer = [];
-  var garbage = [];
+  var check = sequence[0]
+  var result = '';
+  var ray = [];
 
   for (var i = 0; i < sequence.length; i++) {
-    if (check < sequence[i]) {
-      check = sequence[i]
-      garbage.push(sequence[i])
-    } else {
-      answer.push(sequence[i])
-    }
+    
   }
-
-  var compare = 0;
-  for (var i = 0; i < answer.length; i++) {
-    if (compare < answer[i]) {
-      compare = answer[i]
-    }
-  }
-  return compare == answer[answer.length-1] ? true : false;
+  return sequence
 }
 
+console.log(almostIncreasingSequence([10, 1, 2, 3, 4, 5])); // true
+console.log(almostIncreasingSequence([1, 2, 1, 2])); // falsse 1,1,2
 console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
 console.log(almostIncreasingSequence([1, 3, 2])); // true
+console.log(almostIncreasingSequence([5,6,7,8,9]));
