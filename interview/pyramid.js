2348bcc8ca36222a2058b0f469ca922d1cbfb961
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-05 10:26:43 -0600

    triangle practice with doc.write

---
+++ interview/pyramid.js
@@ -0,0 +1,60 @@
+// print all kinds of triangles with #
+
+
+// first triangle
+//   #
+//   ##
+//   ###
+//   ####
+
+// for (var i = '*'; i.length < 10; i+='*') {
+//   console.log(i);
+// }
+
+
+//second triangle
+//   ####
+//   ###
+//   ##
+//   #
+var result = '';
+for (var line = "#"; line.length < 8; line += "#")
+result = line
+  console.log(result);
+
+
+// third  (not a right tri)
+//   #####
+//   ####
+//   ###
+//   ##
+//   #
+  //  for (var i = 0; i < 5; i++) {
+  //     for (var j = 0; j < i; j++) {
+  //       document.write("&nbsp;");
+  //     }
+  //     for (var j = 5; j > i; j--) {
+  //       document.write("*");
+  //       if (j > i + 1) document.write("&nbsp;");
+  //     }
+  //     document.write('<br/>')
+  //   }
+
+
+// fourth
+//      #
+//     ##
+//    ###
+//   ####
+    // for (var i=0; i < 5; i++) {
+    //   for(var j=5; j > i; j--) {
+    //     document.write('&nbsp;');
+    //   }
+    //   for(var j=0; j < i; j++) {
+    //     document.write('*');
+    //     if (j > i) {
+    //       document.write('$nbsp')
+    //     }
+    //   }
+    //   document.write('<br/>')
+    // }
