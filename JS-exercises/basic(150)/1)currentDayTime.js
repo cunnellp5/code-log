38fec090c14f89f6b276f8060c4fddd2a2316acb
pcunnell <philip.cunnell@clickfox.com>
2018-08-23 08:35:45 -0600

    basicJStime exercise

---
+++ JS-exercises/basic(150)/1)currentDayTime.js
@@ -0,0 +1,26 @@
+// Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+// Sample Output : 
+// Today is : Tuesday. 
+// Current time is : 10 PM : 30 : 38
+
+function dayTime() {
+  let dayOfTheWeek = getWeekDay();
+  let time = getTimeOfDay();
+  return dayOfTheWeek + time;
+}
+
+function getTimeOfDay() {
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+  let sec = new Date().getSeconds();
+  let stamp = ((hour >= 12) ? 'PM' : 'AM');
+  return `Current time is : ${hour} ${stamp} : ${min} : ${sec}`;
+}
+
+function getWeekDay() {
+  let week = ['Sunday.', 'Monday.', 'Tuesday.', 'Wednesday.', 'Thursday.', 'Friday.', 'Saturday.'];
+  let weekIndex = new Date().getDay();
+  return `Today is : ${week[weekIndex]} \n`;
+}
+
+console.log(dayTime());
