df3ebf1d6081d123ca48d1738e8c96d37795665a
pcunnell <philip.cunnell@clickfox.com>
2018-09-12 18:07:10 -0600

    working on it

--- JS-exercises/basic(150)/8_GuessingGame/guess.js
+++ JS-exercises/basic(150)/8_GuessingGame/guess.js
@@ -1,19 +1,23 @@
-console.log('hookedup')
+var guessField = document.querySelector('.guessField');
+var guessSubmit = document.querySelector('.guessSubmit');
 
 function randomNumber() {
   min = Math.ceil(1);
   max = Math.floor(11);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
 console.log(randomNumber())
 
-
-
-function inputValue(event) {
-  console.log(event)
-  let input = document.getElementsByTagName('input').value;
-  console.log(input)
-  return input
+function checkGuess() {
+  console.log(randomNumber());
+  console.log(guessField.value);
+  if((randomNumber()) === guessField.value) {
+    return 'HooRAAAAAYYYY👻'
+  } else {
+    return 'guess again sukka'
+  }
 }
-console.log(inputValue());
+console.log(checkGuess());
+
+guessSubmit.addEventListener('click', checkGuess);
+
