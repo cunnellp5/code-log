a5ed470ed68b863c7cc3ac1267104681ae892f84
pcunnell <philip.cunnell@clickfox.com>
2018-09-18 08:17:58 -0600

    added more

--- JS-exercises/basic(150)/10)_multDivide/multDivide.js
+++ JS-exercises/basic(150)/10)_multDivide/multDivide.js
@@ -1,24 +1,35 @@
 
 function multiply() {
   event.preventDefault();
-  let firstNumber = document.getElementById('first').value;
-  let secondNumber = document.getElementById('second').value;
-  let result = firstNumber * secondNumber;
-
+  let firstNumber = parseInt(document.getElementById('first').value);
+  let secondNumber = parseInt(document.getElementById('second').value);
+  var result;
+  if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    result = firstNumber * secondNumber;
+  } else {
+    result = 'no letters, dummy';
+  }
   return document.getElementById('result').innerHTML = result;
 }
 
 function divide() {
   event.preventDefault();
-  let firstNumber = document.getElementById('first').value;
-  let secondNumber = document.getElementById('second').value;
-  let result = firstNumber / secondNumber;
-
-  return document.getElementById('result').innerHTML = result;
+  let firstNumber = parseInt(document.getElementById('first').value);
+  let secondNumber = parseInt(document.getElementById('second').value);
+  let result;
+  if(!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    result = firstNumber / secondNumber;
+  } else {
+    result = 'no letters, dummy';
+  }
+  let setHTML = document.getElementById('result').innerHTML = result;
+  return setHTML;
 }
 
-function clear() {
+function erase() {
   event.preventDefault();
   document.getElementById('first').innerHTML = '';
   document.getElementById('second').innerHTML = '';
+  let result = '** results cleared ** 🤧'
+  return document.getElementById('result').innerHTML = result;
 }
