d163366d7ad1d00d0909f631962675291a415f4a
pcunnell <philip.cunnell@clickfox.com>
2018-09-26 08:24:05 -0600

    soln one

--- JS-exercises/basic(150)/15)_diff13.js
+++ JS-exercises/basic(150)/15)_diff13.js
@@ -1,1 +1,15 @@
-// 15. Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference.
+// 15. Write a JavaScript program to get the difference between a given number and 13, 
+// if the number is greater than 13 return double the absolute difference.
+
+
+function diffWithThirteen(number) {
+  return (number <= 13) ?  (13 - number) : (2 * (number - 13));
+}
+
+
+
+console.log(diffWithThirteen(4))
+console.log(diffWithThirteen(8))
+console.log(diffWithThirteen(12))
+console.log(diffWithThirteen(16))
+console.log(diffWithThirteen(13))
