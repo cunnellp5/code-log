c7897627d0495e42cc3fe53e7f090c59e208da69
pcunnell <philip.cunnell@clickfox.com>
2018-09-27 09:01:35 -0600

    silly math function but wired up full css html and js with buttons and inputs

--- JS-exercises/basic(150)/16)_sumToTriple/main.js
+++ JS-exercises/basic(150)/16)_sumToTriple/main.js
@@ -1,1 +1,42 @@
-console.log('hi')
+
+var inputOne = document.createElement('input');
+    inputOne.setAttribute('id', 'one');
+    inputOne.setAttribute('placeholder', 'first integer');
+    inputOne.setAttribute('type', 'number');
+
+var inputTwo = document.createElement('input');
+    inputTwo.setAttribute('id', 'two');
+    inputTwo.setAttribute('placeholder', 'second integer');
+    
+var button = document.createElement('button');
+    button.innerText = 'add!'
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', addTwoTriple)
+    
+var clear = document.createElement('button');
+    clear.innerText = 'clear'
+    clear.setAttribute('type', 'button');
+    clear.addEventListener('click', erase)
+
+var result = document.createElement('p')
+    result.innerText = 'Result:'
+
+var parent = document.getElementsByTagName('body')[0];
+    parent.append(inputOne, inputTwo, button, clear, result)
+
+
+
+
+function erase() {
+  inputOne.value = '';
+  inputTwo.value = '';
+  result.innerText = 'Result:'
+}
+
+function addTwoTriple() {
+  let val1 = parseInt(inputOne.value);
+  let val2 = parseInt(inputTwo.value);
+  let answer = (val1 === val2) ? (3 * (val1 + val2)) : (val1 + val2);
+  return result.innerText += ' ' + answer;
+}
+
