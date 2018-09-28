0ba188d1eb9f37a1988e1f0acbc33bfa488a43ba
pcunnell <philip.cunnell@clickfox.com>
2018-09-28 09:13:37 -0600

    keeping the train going

---
+++ JS-exercises/basic(150)/17-?/20.js
@@ -0,0 +1,46 @@
+// 20. Write a JavaScript program to check from two given integers, if one is positive and one is negative
+var javaScriptDiv = document.createElement('div');
+javaScriptDiv.innerText = '20. Write a JavaScript program to check from two given integers, if one is positive and one is negative'
+
+function posAndNeg(first, second) {
+  if (typeof(first || second) !== 'number') return false
+  let check1 = first >= 0;
+  let check2 = second >= 0;
+  return check1 !== check2;
+}
+
+console.log(posAndNeg(3, 4)) // false
+console.log(posAndNeg(3, -4)) // true
+console.log(posAndNeg(3, 0)) // false
+console.log(posAndNeg(3, 30)) // false
+console.log(posAndNeg(-4, -1)) // false
+console.log(posAndNeg(undefined, null)) // false
+console.log(posAndNeg('poop', 3)) // false
+
+var javaScriptDivPre = document.createElement('pre')
+javaScriptDivPre.innerHTML = 
+`<code>
+function posAndNeg(first, second) {
+  if (typeof(first || second) !== 'number') return false
+  let check1 = first >= 0;
+  let check2 = second >= 0;
+  return check1 !== check2;
+}
+
+console.log(posAndNeg(3, 4)) // false
+console.log(posAndNeg(3, -4)) // true
+console.log(posAndNeg(3, 0)) // false
+console.log(posAndNeg(3, 30)) // false
+console.log(posAndNeg(-4, -1)) // false
+console.log(posAndNeg(undefined, null)) // false
+console.log(posAndNeg('poop', 3)) // false
+
+  </code>`;
+
+javaScriptDiv.appendChild(javaScriptDivPre);
+var hr19post = document.createElement('hr');
+
+
+var body = document.getElementsByTagName('body')[0];
+body.append(javaScriptDiv, hr19post);
+
