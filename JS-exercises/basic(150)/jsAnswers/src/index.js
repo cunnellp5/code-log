65ce4a04e7e67db6d693dbfc45ae64cff36a4de7
pcunnell <philip.cunnell@clickfox.com>
2018-10-03 08:55:16 -0600

    code splitting and lazy loading experiment

--- JS-exercises/basic(150)/jsAnswers/src/index.js
+++ JS-exercises/basic(150)/jsAnswers/src/index.js
@@ -12,4 +12,21 @@ function createPage(ray) {
   })
 }
 
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+  var br = document.createElement('br');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console!';
+  element.appendChild(br);
+  element.appendChild(btn);
+
+  btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+    print();
+  });
+  return element;
+}
+
+document.body.appendChild(component());
 createPage(masterArray)
