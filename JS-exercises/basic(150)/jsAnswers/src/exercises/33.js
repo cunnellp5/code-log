dd73a36c891de53244471a881124e2e356285973
pcunnell <philip.cunnell@clickfox.com>
2018-10-17 08:11:55 -0600

    console err

--- JS-exercises/basic(150)/jsAnswers/src/exercises/33.js
+++ JS-exercises/basic(150)/jsAnswers/src/exercises/33.js
@@ -29,7 +29,7 @@ console.log(rangerWillRobinson(40, 111)); // false
 
     </code>`;
 
-function rangerWillRobinson(...numbers) {
+function rangerWillRobinson1(...numbers) {
   ray = [];
   numbers.forEach((el) => {
     if((el >= 40 && el <= 60) || (el <= 100 && el >= 70)) {
