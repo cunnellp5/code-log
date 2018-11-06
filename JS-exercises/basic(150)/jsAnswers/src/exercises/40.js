8c06f49e750489382d09208b105a6da063a84e22
cunnellp5 <philip.cunnell@colorado.edu>
2018-11-06 07:50:42 -0700

    add and subtract and check

--- JS-exercises/basic(150)/jsAnswers/src/exercises/40.js
+++ JS-exercises/basic(150)/jsAnswers/src/exercises/40.js
@@ -1,10 +1,35 @@
-const question = `QUESTION GOES HERE`;
+const question = `40. Write a JavaScript program to check from two given integers if either one is 8 or their sum or difference is 8.`;
 const blockcode =`<code>
-    COPY AND PASTE CODE FROM BELOW RIGHT HERE TO RENDER IN HTML
+function sumDiff(one, two) {
+  if(one === 8 || two === 8) {
+    return true;
+  } else if (one + two === 8) {
+    return true;
+  } else if (Math.abs(one - two) === 8) {
+    return true;
+  } else {
+    return false
+  }
+
+}
+
+console.log(sumDiff(7, 8)); // true
+console.log(sumDiff(16, 8)); // true
+console.log(sumDiff(24, 32)); // true
+console.log(sumDiff(17, 18)); // false
     </code>`;
 
-function template(arguments) {
-  // CODE HERE
+function sumDiff(one, two) {
+  if(one === 8 || two === 8) {
+    return true;
+  } else if (one + two === 8) {
+    return true;
+  } else if (Math.abs(one - two) === 8) {
+    return true;
+  } else {
+    return false
+  }
+
 }
 
 export { question, blockcode }
