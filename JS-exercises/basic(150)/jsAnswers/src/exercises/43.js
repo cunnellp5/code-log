ef1a6d970f3adea5ef826b25c7dd3622753e28ad
cunnellp5 <philip.cunnell@colorado.edu>
2018-11-08 07:33:48 -0700

    reformatting files and imports then compare last digit of three integers

--- JS-exercises/basic(150)/jsAnswers/src/exercises/43.js
+++ JS-exercises/basic(150)/jsAnswers/src/exercises/43.js
@@ -1,10 +1,30 @@
-// const question = `QUESTION GOES HERE`;
-// const blockcode =`<code>
-//     COPY AND PASTE CODE FROM BELOW RIGHT HERE TO RENDER IN HTML
-//     </code>`;
-//
-// function template(arguments) {
-//   // CODE HERE
-// }
-//
-// export { question, blockcode }
+const question = `43. Write a JavaScript program to check from three given numbers (non negative integers) that two or all of them have the same rightmost digit.`;
+const blockcode =`<code>
+function rightdigit(...numbers) {
+  let lastDigitArray = numbers.map((el) => {
+    var numberRay = el.toString().split('');
+    var lastDig = +numberRay[numberRay.length -1];
+    return lastDig;
+  })
+  return lastDigitArray.every(el => {
+    return el === lastDigitArray[0];
+  })
+}
+
+console.log(rightdigit(22,32,42)); // true
+console.log(rightdigit(102,302,2)); // true
+console.log(rightdigit(20,22,45)); // false
+    </code>`;
+
+function rightdigit(...numbers) {
+  let lastDigitArray = numbers.map((el) => {
+    var numberRay = el.toString().split('');
+    var lastDig = +numberRay[numberRay.length -1];
+    return lastDig;
+  })
+  return lastDigitArray.every(el => {
+    return el === lastDigitArray[0];
+  })
+}
+
+export { question, blockcode }
--- JS-exercises/basic(150)/jsAnswers/src/exercises/main.js
+++ JS-exercises/basic(150)/jsAnswers/src/exercises/main.js
@@ -1,38 +1,38 @@
 // This file is just all the info from the above files mashed into one
-// import * as seventeen from './teens/17';
-// import * as eighteen from './teens/18';
-// import * as ninteen from './teens/19';
-import * as teens from './teens/index';
-import * as twenty from './20';
-import * as twentyone from './21';
-import * as twentytwo from './22';
-import * as twentythree from './23';
-import * as twentyfour from './24';
-import * as twentyfive from './25';
-import * as twentysix from './26';
-import * as twentyseven from './27';
-import * as twentyeight from './28';
-import * as twentynine from './29';
-import * as thirty from './30';
-import * as thirtyone from './31';
-import * as thirtytwo from './32';
-import * as thirtythree from './33';
-import * as thirtyfour from './34';
-import * as thirtyfive from './35';
-import * as thirtysix from './36';
-import * as thirtyseven from './37';
-import * as thirtyeight from './38';
-import * as thirtynine from './39';
+import * as seventeen from './teens/17';
+import * as eighteen from './teens/18';
+import * as ninteen from './teens/19';
+// import * as teens from './teens/index';
+import * as twenty from './twenties/20';
+import * as twentyone from './twenties/21';
+import * as twentytwo from './twenties/22';
+import * as twentythree from './twenties/23';
+import * as twentyfour from './twenties/24';
+import * as twentyfive from './twenties/25';
+import * as twentysix from './twenties/26';
+import * as twentyseven from './twenties/27';
+import * as twentyeight from './twenties/28';
+import * as twentynine from './twenties/29';
+import * as thirty from './thirties/30';
+import * as thirtyone from './thirties/31';
+import * as thirtytwo from './thirties/32';
+import * as thirtythree from './thirties/33';
+import * as thirtyfour from './thirties/34';
+import * as thirtyfive from './thirties/35';
+import * as thirtysix from './thirties/36';
+import * as thirtyseven from './thirties/37';
+import * as thirtyeight from './thirties/38';
+import * as thirtynine from './thirties/39';
 import * as forty from './40';
 import * as fortyone from './41';
 import * as fortytwo from './42';
-// import * as fortythree from './43';
+import * as fortythree from './43';
 
 
 const masterArray = [
-  // seventeen,
-  // eighteen,
-  // ninteen,
+  seventeen,
+  eighteen,
+  ninteen,
   twenty,
   twentyone,
   twentytwo,
@@ -56,13 +56,15 @@ const masterArray = [
   forty,
   fortyone,
   fortytwo,
-  // fortythree
+  fortythree
 ]
 
-const teenagers = teens.teens.forEach((el) => {
-  masterArray.push({ blockcode: el.blockcode, question: el.question })
-})
-console.log(teenagers)
+// const teenagers = teens.teens.forEach((el) => {
+//   masterArray.unshift({ blockcode: el.blockcode, question: el.question })
+// })
+// console.log(teenagers)
+
+// create an import array, then an extraction function like above to push the nested blockcode and question object
 
 
 export { masterArray };
