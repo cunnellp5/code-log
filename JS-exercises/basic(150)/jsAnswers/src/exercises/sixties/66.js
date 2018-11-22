bd0895b82ae30f3dab5470598a6b8d5c7cc1fc8e
cunnellp5 <philip.cunnell@colorado.edu>
2018-11-22 13:25:45 -0500

    warm up

---
+++ JS-exercises/basic(150)/jsAnswers/src/exercises/sixties/66.js
@@ -0,0 +1,16 @@
+const question = `66. Write a JavaScript program to display the city name if the string begins with "Los" or "New" otherwise return blank.`;
+const blockcode =`<code>
+function city_name(string) {
+  return string.startsWith('Los') || string.startsWith('New') ? string : '';
+}
+
+console.log(city_name("New York")); // New York
+console.log(city_name("Los Angeles")); // Los Angles
+console.log(city_name("London")); //
+    </code>`;
+
+function city_name(string) {
+  return string.startsWith('Los') || string.startsWith('New') ? string : '';
+}
+
+export { question, blockcode }
