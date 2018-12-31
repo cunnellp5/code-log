97cdbcaed2c96a43ca9c58736e0073374a0aba2c
cunnellp5 <philip.cunnell@colorado.edu>
2018-12-30 18:30:24 -0700

    random color checkerboard

--- ~school/checkerboard/script.js
+++ ~school/checkerboard/script.js
@@ -1,37 +1,38 @@
-// Your JS goes here
-
 $(document).ready(function(){
 
-// for (var i = 0; i < 31; i++) {
-//   $('body').append('<div class="red"></div>');
-//   $('body').append('<div class="black"></div>');
-// }
-//
-// $('div').css('width', '11.1%').css('padding-bottom', '11.1%').css('background-color','red').css('float','left');
-// $('.black').css('background-color', 'black');
+// ###Checkerboard
+  // var dimension = 30;
+  // var percentage = 100/dimension;
+  //   for (var i = 0; i < dimension; i++) {
+  //     for (var j = 0; j < dimension; j++) {
+  //       if ((i + j) % 2 == 0) {
+  //         $('body').append('<div class="red"></div>')
+  //         $('.red').css('background-color', 'red').width(percentage + '%').css('padding-bottom', percentage + '%').css('float', 'left')
+  //       } else {
+  //         $('body').append('<div class="black"></div>')
+  //         $('.black').css('background-color', 'black').width(percentage + '%').css('padding-bottom', percentage + '%').css('float', 'left')
+  //     }
+  //   }
+  // }
 
-
-//OOOORRRRRRR
-
-var dimension = 30;
-var percentage = 100/dimension;
-  for (var i = 0; i < dimension; i++) {
-    for (var j = 0; j < dimension; j++) {
-      if ((i + j) % 2 == 0) {
-        $('body').append('<div class="red"></div>')
-        $('.red').css('background-color', 'red').width(percentage + '%').css('padding-bottom', percentage + '%').css('float', 'left')
-      } else {
-        $('body').append('<div class="black"></div>')
-        $('.black').css('background-color', 'black').width(percentage + '%').css('padding-bottom', percentage + '%').css('float', 'left')
+  // ###Random Colors
+  var dimension = 14;
+  var percentage = 100 / dimension;
+    for (var i = 0; i < dimension; i++) {
+      for (var j = 0; j < dimension; j++) {
+          let body = $('body');
+          let div = $('<div></div>')
+              .css('background-color', randomColor())
+              .width(percentage + '%')
+              .css('padding-bottom', percentage + '%')
+              .css('float', 'left');
+          body.append(div)
+      }
     }
-  }
-}
+    function randomColor() {
+        var color = Math.floor(0x1000000 * Math.random()).toString(16);
+        return '#' + ('000000' + color).slice(-6);
+    }
+
 
 });
-
-
-// * Each tile should be a `div`
-// * Each tile's width is `11.1%`
-// * Set each tile's `float` property to `left`
-// * Each tile's paddingBottom is `11.1%`
-//
