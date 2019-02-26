071ba2d6772f74dca9ca7a3e3f132ada7fd2f764
pcunnell <philip.cunnell@clickfox.com>
2019-02-25 19:21:34 -0700

    set up next exercicio

--- wes-bos-30days/05-flex-panels-image-gallery/main.js
+++ wes-bos-30days/05-flex-panels-image-gallery/main.js
@@ -5,11 +5,9 @@ function toggleOpen() {
 }
 
 function toggleActive(e) {
-  console.log(e.propertyName)
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
   }
-  // this.classList.toggle('open-active');
 }
 
 panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
