ea1edd2f1fd853797d5dd829dd5643b2d9aa6625
pcunnell <philip.cunnell@clickfox.com>
2019-02-25 21:19:19 -0700

    attempt at getting closest lat lon city

--- wes-bos-30days/06-ajax-type-ahead/main.js
+++ wes-bos-30days/06-ajax-type-ahead/main.js
@@ -1,9 +1,18 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => cities.push(...data));
+window.onload = function() {
+  fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(UserLocation);
+    }
+    // Default to Washington, DC
+    else
+      NearestCity(38.8951, -77.0367);
+}
 
 function findMatches(wordToMatch, cities) {
   return cities.filter(place => {
@@ -24,8 +33,11 @@ function displayMatches() {
     const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
     return `
       <li>
-        <span class='name'>${cityName}, ${stateName}</span>
-        <span class='population'>${numberWithCommas(place.population)}</span>
+        <div class='wrap'>
+          <span class='name'>${cityName}, ${stateName}</span>
+          <span class='population'>${numberWithCommas(place.population)}</span>
+        </div>
+          <small class='distance'>${closest} km</small>
       </li>
     `
   }).join('');
@@ -37,3 +49,45 @@ const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches)
 searchInput.addEventListener('keyup', displayMatches)
+
+
+// Callback function for asynchronous call to HTML5 geolocation
+function UserLocation(position) {
+  NearestCity(position.coords.latitude, position.coords.longitude);
+}
+
+// Convert Degress to Radians
+function Deg2Rad(deg) {
+  return deg * Math.PI / 180;
+}
+
+function PythagorasEquirectangular(lat1, lon1, lat2, lon2) {
+  lat1 = Deg2Rad(lat1);
+  lat2 = Deg2Rad(lat2);
+  lon1 = Deg2Rad(lon1);
+  lon2 = Deg2Rad(lon2);
+  var R = 6371; // km
+  var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+  var y = (lat2 - lat1);
+  var d = Math.sqrt(x * x + y * y) * R;
+  return d;
+}
+
+function NearestCity(latitude, longitude) {
+  var minDif = 99999;
+  var closest;
+
+  for (index = 0; index < cities.length; ++index) {
+    var dif = PythagorasEquirectangular(latitude, longitude, cities[index].latitude, cities[index].longitude);
+    if (dif < minDif) {
+      closest = index;
+      minDif = dif;
+    }
+  }
+
+  // echo the nearest city
+  console.log(closest, 'closest');
+  console.log(minDif, 'minDif');
+  console.log('closest', cities[closest].city);
+  // alert(cities[closest]);
+}
