27fae60c2627407e647c421ccdc775f299445868
pcunnell <philip.cunnell@clickfox.com>
2019-02-26 09:02:06 -0700

    tiny refactor

--- wes-bos-30days/07-array-cardio-day2/main.js
+++ wes-bos-30days/07-array-cardio-day2/main.js
@@ -13,12 +13,12 @@
       { text: 'Nice Nice Nice!', id: 542328 }
     ];
     // Some and Every Checks
-    const thisYear = new Date().getUTCFullYear();
+    const thisYear = new Date().getFullYear();
     // Array.prototype.some() // is at least one person 19 or older?
-    const greaterThan19 = people.some((person) => person.year > (thisYear - 19));
-    console.log(greaterThan19)
+    const greaterThan19 = people.some((person) => thisYear - person.year >= 19);
+    console.log({greaterThan19})
     // Array.prototype.every() // is everyone 19 or older?
-    
+
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
