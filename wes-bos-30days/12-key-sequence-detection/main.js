4c2baaf8f022a11c4ad87c9886a2280827ef6cc5
pcunnell <philip.cunnell@clickfox.com>
2019-03-08 18:33:41 -0700

    found another cornify konami code in source code

--- wes-bos-30days/12-key-sequence-detection/main.js
+++ wes-bos-30days/12-key-sequence-detection/main.js
@@ -1,10 +1,13 @@
+// require('http://www.cornify.com/js/cornify.js');
 const pressed = [];
 const secretCode = 'philbear';
 
-window.addEventListener('keyup', (e) => {
+window.addEventListener('keyup', (e) => {  
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  console.log(pressed)
   if(pressed.join('').includes(secretCode)) {
-    cornify_add();
+    // cornify_add();
+    cornami.input = "38384040373937396665"
   }
 })
