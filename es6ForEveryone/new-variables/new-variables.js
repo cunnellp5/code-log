17a025767a4a179ad6563906c4874ba3797217ed
pcunnell <philip.cunnell@clickfox.com>
2019-12-17 07:12:38 -0700

    let and const differences

--- es6ForEveryone/new-variables/new-variables.js
+++ es6ForEveryone/new-variables/new-variables.js
@@ -1,15 +1,32 @@
 
-function setWidth() {
-  var width = 100;
-  console.log(width);
-}
-setWidth();
+// function setWidth() {
+//   var width = 100;
+//   console.log(width);
+// }
+// setWidth();
 
-var age = 100;
-if (age > 12) {
-  const dogYears = age * 7;
-  console.log(`you are ${dogYears} dog years old!`)
-}
+// var age = 100;
+// if (age > 12) {
+//   const dogYears = age * 7;
+//   console.log(`you are ${dogYears} dog years old!`)
+// }
 
-let height = 200;
+// let height = 200;
+
 const key = 'abc123';
+
+let points = 50;
+let winner = false;
+points = 59;
+
+const person = {
+  name: 'phil',
+  age: 22
+}
+
+const phil = Object.freeze(person);
+
+// if (points > 40) {
+//   console.log('ran')
+//   let winner = true;
+// }
