ecfb0b474b3e8883bb3701caff4b7228c1e31a2e
cunnellp5 <philip.cunnell@colorado.edu>
2020-01-04 12:16:51 -0700

    arrow practice

--- es6ForEveryone/arrow-functions/arrow-functions.js
+++ es6ForEveryone/arrow-functions/arrow-functions.js
@@ -1,20 +1,18 @@
-const names = ['phil', 'arms', 'jazz'];
-
+const names = ["phil", "arms", "jazz"];
 
 const fullNamesOne = names.map(function(name) {
   return `${name} bos`;
 });
 
-const fullNames2 = names.map((name) => {
+const fullNames2 = names.map(name => {
   return `${name} bos`;
 });
 
-const fullNames3 = names.map((name) => `${name} bos`);
+const fullNames3 = names.map(name => `${name} bos`);
 
 const fullNames4 = names.map(name => `${name} bos`);
 
 const fullNames5 = names.map(() => `cool boss`);
-
 
 // console.log(fullNamesOne5);
 
@@ -22,8 +20,24 @@ const fullNames5 = names.map(() => `cool boss`);
 //   alert('hello' + name);
 // }
 
-const sayMyName = (name) => {
-  alert('hello ' + name);
-}
+const sayMyName = name => {
+  alert("hello " + name);
+};
 
-sayMyName('flip')
+// sayMyName("flip");
+
+const race = "100m dash";
+const winners = ["hunter gath", "singag song", "simagbos"];
+
+const win = winners.map((winner, i) => ({
+  name: winner,
+  race,
+  place: i + 1
+}));
+
+console.table(win);
+
+const ages = [12, 14, 42, 63, 74, 24, 13, 24, 62, 84, 96, 75, 355];
+const old = ages.filter(age => age >= 60);
+
+console.log(old);
