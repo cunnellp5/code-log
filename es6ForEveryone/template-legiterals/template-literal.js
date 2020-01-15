7ca0283b9c5200bdad8f276cc4217ca2d8fca8b9
cunnellp5 <philip.cunnell@colorado.edu>
2020-01-15 08:39:25 -0700

    loopin and nesting inside of template literals

--- es6ForEveryone/template-legiterals/template-literal.js
+++ es6ForEveryone/template-legiterals/template-literal.js
@@ -1,3 +1,35 @@
 const name = 'Snickers';
 const age = 2;
-const sentence = `my dog ${name} is  ${age * 7} years old`
+const sentence = `my dog ${name} is  ${age * 7} years old`;
+
+const person = {
+    name: 'phil',
+    job: 'poop collector',
+    city: 'poopville',
+    bio: 'no thanks'
+};
+
+// const markup = `
+//     <div class="person">
+//         <h2>
+//             ${person.name}
+//             <span class="job">${person.job}</span>
+//         </h2>
+//         <p class="location">${person.city}</p>
+//         <p class="location">${person.bio}</p>
+//     </div>
+// `;
+
+const dogs = [
+    {name: 'snicker', age: 3},
+    {name: 'joe', age: 23},
+    {name: 'bo', age: 5},
+];
+
+const markup = `
+    <ul class="dogs">
+        ${dogs.map(dog => `<li>${dog.name} is ${dog.age * 7}</li>`).join('')}
+    </ul>
+`
+
+document.body.innerHTML = markup;
