b0f5787f42462d48f54f884f90954d8a5ae9a3fa
cunnellp5 <philip.cunnell@colorado.edu>
2020-01-22 08:05:58 -0700

    destructuring practice

--- es6ForEveryone/destructuring/destructureObj.js
+++ es6ForEveryone/destructuring/destructureObj.js
@@ -6,4 +6,29 @@ const person = {
     twitter: '@philbear'
 }
 
-const {first, last} = person;
+const { first, last, twitter } = person;
+
+const phil = {
+    first: 'flip',
+    last: 'bear',
+    links: {
+        social: {
+            facebook: 'https://stuff',
+            twitter: 'https://other'
+        },
+        web: {
+            blog: 'https://philbear'
+        }
+    }
+}
+
+const { facebook: fb, twitter: tweet } = phil.links.social;
+
+const settings = {
+    width: 300,
+    color: 'black'
+} // height, font-size
+
+// const { width = 100, height = 100, color = 'blue', fontsize = 25 } = settings;
+
+const { w: width = 400, h: height = 500 } = { w: 800 };
