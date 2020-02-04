f8eef38c2cef53920eb2d7ac6e477d314965434c
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-03 17:07:28 -0700

    swapping vars

--- es6ForEveryone/destructuring/arrays.js
+++ es6ForEveryone/destructuring/arrays.js
@@ -1,10 +1,28 @@
-const details = ["poo", "py", 1333];
-const [name, nametwo, id] = details;
+// const details = ["poo", "py", 1333];
+// const [name, nametwo, id] = details;
 
-console.log(name, nametwo, id);
+// console.log(name, nametwo, id);
 
-const data = "basketball, sport, 94949, 11, poo, wbo, bose";
-const [itemName, category, sku, inventory] = data.split(",");
+// const data = "basketball, sport, 94949, 11, poo, wbo, bose";
+// const [itemName, category, sku, inventory] = data.split(",");
 
-const team = ["wes", "merry", "sarah", "keegan", "riker"];
-const [captain, assistant, ...players] = team;
+// const team = ["wes", "merry", "sarah", "keegan", "riker"];
+// const [captain, assistant, ...players] = team;
+
+
+let inring = 'hulk hogan';
+let onside = 'the rock';
+
+// old way
+// var tmp = inring;
+// inring = onside;
+// onside = tmp;
+// delete tmp;
+
+// new way
+
+console.log(inring, onside);
+
+[inring, onside] = [onside, inring];
+
+console.log(inring, onside);
