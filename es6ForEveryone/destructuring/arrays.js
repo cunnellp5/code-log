f9742796293afd16ca0cd33ad4245f48608e1c74
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-04 19:56:17 -0700

    destructuring functions

--- es6ForEveryone/destructuring/arrays.js
+++ es6ForEveryone/destructuring/arrays.js
@@ -1,3 +1,4 @@
+// strict mode
 // const details = ["poo", "py", 1333];
 // const [name, nametwo, id] = details;
 
@@ -10,19 +11,49 @@
 // const [captain, assistant, ...players] = team;
 
 
-let inring = 'hulk hogan';
-let onside = 'the rock';
+// let inring = 'hulk hogan';
+// let onside = 'the rock';
 
-// old way
-// var tmp = inring;
-// inring = onside;
-// onside = tmp;
-// delete tmp;
+// // old way
+// // var tmp = inring;
+// // inring = onside;
+// // onside = tmp;
+// // delete tmp;
 
-// new way
+// // new way
 
-console.log(inring, onside);
+// console.log(inring, onside);
 
-[inring, onside] = [onside, inring];
+// [inring, onside] = [onside, inring];
 
-console.log(inring, onside);
+// console.log(inring, onside);
+
+// canadian base
+function convertCurrency(amount) {
+    const converted = {
+        UDS: amount * .76,
+        GPB: amount * .53,
+        AUD: amount * 1.01,
+        MEX: amount * 13.36,
+    };
+    return converted;
+}
+// // canadian base
+// const hundo = convertCurrency(100);
+// console.log(hundo.AUD);
+// console.log(hundo.GPB);
+// console.log(hundo.MEX);
+
+// destructuring immediagely
+// const { UDS, GPB, AUD, MEX } = convertCurrency(100);
+
+function tipCalc({total = 100, tip = 0.15, tax = 0.13} = {}) {
+    return total + (tip * total) + (tax * total);
+}
+
+// const bill = tipCalc({ total: 200, tip: 0.20, tax: 0.13 });
+// const bill = tipCalc({ total: 200, tip: 0.20 });
+// const bill = tipCalc({ total: 200 });
+const obj = { total: 200, tip: 0.20, tax: 0.13 }
+const bill = tipCalc(obj);
+console.log(bill);
