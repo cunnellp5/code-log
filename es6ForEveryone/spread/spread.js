803eba79b83a8775781437025deb14b1aa5bc568
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-10 09:40:32 -0700

    spreading and animation nation

--- es6ForEveryone/spread/spread.js
+++ es6ForEveryone/spread/spread.js
@@ -1,14 +1,28 @@
-const featured = ['Deep Dish', 'Pepperoni', 'Hawaiian'];
-const specialty = ['Meatzza', 'Spicy Mama', 'Margherita'];
+// const featured = ['Deep Dish', 'Pepperoni', 'Hawaiian'];
+// const specialty = ['Meatzza', 'Spicy Mama', 'Margherita'];
 
-// const pizzas = featured.concat(specialty);
-let pizzas = [...featured, 'veg', ...specialty];
-// const fridayPizzas = [].concat(pizzas)
-const fridayPizzas = [...pizzas];
-console.log(pizzas)
+// // const pizzas = featured.concat(specialty);
+// let pizzas = [...featured, 'veg', ...specialty];
+// // const fridayPizzas = [].concat(pizzas)
+// const fridayPizzas = [...pizzas];
+// console.log(pizzas)
 
-const deepDish = {
-  pizzaName: 'Deep Dish',
-  size: 'Medium',
-  ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
-};
+// const deepDish = {
+//   pizzaName: 'Deep Dish',
+//   size: 'Medium',
+//   ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
+// };
+
+// const title = Array.from(document.querySelector('.jump'), (title) => {
+//     const word = title.textContent;
+    
+//     console.log([...word])
+//     return title
+// });
+const title = document.querySelector('.jump');
+title.innerHTML = sparanWrap(title.textContent);
+
+
+function sparanWrap(word) {
+    return [...word].map(letter => `<span>${letter}</span>`).join('');
+}
