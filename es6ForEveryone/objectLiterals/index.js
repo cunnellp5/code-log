385080665eb1b6237f3dfcfd2137abbd46c19ec5
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-15 15:14:00 -0700

    assigning dynamic keys to an ojbect

--- es6ForEveryone/objectLiterals/index.js
+++ es6ForEveryone/objectLiterals/index.js
@@ -24,3 +24,27 @@ const modal = {
 
     }
 }
+
+const invertColor = (color) => {
+    return '#' + ('000000' + (0xFFFFFF ^ parseInt(color.substring(1), 16)).toString(16)).slice(-6);
+}
+
+const key = 'pocketColor';
+const value = '#ffc600';
+const tShirt = {
+    [key]: value,
+    [`${key}Opposite`]: invertColor(value)
+}
+
+console.log(tShirt)
+
+const keys = ['xize', 'color', 'wight'];
+const values = ['medium', 'red', 222];
+
+const shirt = {
+    [keys.shift()]: values.shift(),
+    [keys.shift()]: values.shift(),
+    [keys.shift()]: values.shift(),
+}
+
+console.log(shirt)
