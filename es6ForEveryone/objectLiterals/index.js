3aed6f69f65c4929322f834ec518d969793e81e5
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-15 15:06:01 -0700

    updates to object legiterals

---
+++ es6ForEveryone/objectLiterals/index.js
@@ -0,0 +1,26 @@
+const first = 'snicmers';
+const last = 'box';
+const age = 2;
+const breed = 'ing charse cav ';
+
+const dog = {
+    first,
+    last,
+    age,
+    breed,
+    pals: ['hugo', 'scummy']
+}
+
+// console.log(dog);
+
+const modal = {
+    create(selector) {
+
+    },
+    open(content) {
+
+    },
+    close(goodbye) {
+
+    }
+}
