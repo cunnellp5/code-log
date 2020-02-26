4fbcfdee5c47a1c5e653690d78bafecf45ff9251
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-25 22:39:18 -0700

    resolving and rejecting errors

--- es6ForEveryone/promises/promises.js
+++ es6ForEveryone/promises/promises.js
@@ -1,6 +1,20 @@
-const postsPromise = fetch('');
+// const postsPromise = fetch('');
 
-postsPromise
-    .then(data => data.json())
-    .then((data => { console.log(data) }))
-    .catch(error => { console.error(error) })
+// postsPromise
+//     .then(data => data.json())
+//     .then((data => { console.log(data) }))
+//     .catch(error => { console.error(error) })
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(Error('error will robinsont'));
+    }, 1000)
+});
+
+p
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error)
+    })
