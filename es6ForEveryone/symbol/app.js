3588032ddc9130bd2e43cfe27beca53eaca1cbcd
cunnellp5 <philip.cunnell@colorado.edu>
2020-02-25 23:28:44 -0700

    symbols

---
+++ es6ForEveryone/symbol/app.js
@@ -0,0 +1,8 @@
+const phil = Symbol('phil');
+const person = Symbol('phil');
+const classRoom = {
+    [Symbol('mark')]: {grade: 50, gender: 'male'},
+    [Symbol('olivia')]: {grade: 30, gender: 'female'},
+    [Symbol('olivia')]: {grade: 30, gender: 'female'},
+}
+
